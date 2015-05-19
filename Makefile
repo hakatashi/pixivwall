70700246e2e10a2ed299a233a41abd49e168588f
Koki Takahashi <hakatasiloving@gmail.com>
2015-05-19 16:38:16 +0900

    Change to take files from arguments

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
-LIBS = ole32.lib user32.lib shell32.lib
+LIBS = ole32.lib user32.lib shell32.lib Shlwapi.lib Winhttp.lib Pathcch.lib
 
-all: main.exe
+all: pixivwall.exe
 
 main.obj: main.cpp
 	cl main.cpp /c /Fomain.obj
 
-main.exe: main.obj
-	cl main.obj $(LIBS) /Femain.exe
+pixivwall.exe: main.obj
+	cl main.obj $(LIBS) /Fepixivwall.exe
--- main.cpp
+++ main.cpp
@@ -4,13 +4,18 @@
 #define STRICT_TYPED_ITEMIDS
 
 #include <windows.h>
+#include <winhttp.h>
 #include <Shlobj.h>
+#include <Shlwapi.h>
+#include <Pathcch.h>
 #include <shobjidl.h>
 #include <wrl/client.h>
 #include <stdio.h>
 
 #define LOG(format, ...) wprintf(format L"\n", __VA_ARGS__)
 #define ERRORED(line) wprintf(L"Error at line %d\n", line)
+
+#define MAX_IMAGES 256
 
 class CoUninitializeOnExit {
 public:
@@ -33,6 +38,8 @@ HRESULT CreateShellItemArrayFromPaths(UINT ct, LPCWSTR rgt[], IShellItemArray **
     PIDLIST_ABSOLUTE *rgpidl = new PIDLIST_ABSOLUTE[ct];
     HRESULT hr = rgpidl ? S_OK : E_OUTOFMEMORY;
 
+    if (FAILED(hr)) return ERRORED(__LINE__);
+
     int cpidl;
     for (cpidl = 0; SUCCEEDED(hr) && cpidl < ct; cpidl++) {
         hr = SHParseDisplayName(rgt[cpidl], nullptr, &rgpidl[cpidl], 0, nullptr);
@@ -51,24 +58,33 @@ HRESULT CreateShellItemArrayFromPaths(UINT ct, LPCWSTR rgt[], IShellItemArray **
 }
 
 int _cdecl wmain(int argc, LPCWSTR argv[]) {
-    LPCWSTR paths[] = {
-        L"C:\\Users\\hakatashi\\Documents\\GitHub\\pixivwall\\images\\01.jpg",
-        L"C:\\Users\\hakatashi\\Documents\\GitHub\\pixivwall\\images\\02.jpg",
-        L"C:\\Users\\hakatashi\\Documents\\GitHub\\pixivwall\\images\\03.jpg",
-        L"C:\\Users\\hakatashi\\Documents\\GitHub\\pixivwall\\images\\04.jpg",
-        L"C:\\Users\\hakatashi\\Documents\\GitHub\\pixivwall\\images\\05.jpg"
-    };
+    LPCWSTR paths[MAX_IMAGES];
 
+    WCHAR dirname[MAX_PATH];
+    GetCurrentDirectory(MAX_PATH, dirname);
+
+    LOG(L"Current directory is %s", dirname);
+
+    for (int i = 1; i < argc; i++) {
+        WCHAR path[MAX_PATH];
+
+        PathCchCombine(path, MAX_PATH, dirname, argv[i]);
+        paths[i - 1] = path;
+        LOG(L"Image%02d: %s", i, path);
+    }
+
+    // Initialize COM Interface
     HRESULT hr = CoInitialize(nullptr);
     if (FAILED(hr)) return ERRORED(__LINE__);
 
+    // Create instance of IDesktopWallpaper
     IDesktopWallpaper *pDesktopWallpaper = nullptr;
     hr = CoCreateInstance(__uuidof(DesktopWallpaper), nullptr, CLSCTX_ALL, IID_PPV_ARGS(&pDesktopWallpaper));
     if (FAILED(hr)) return ERRORED(__LINE__);
     ReleaseOnExit releaseDesktopWallpaper(pDesktopWallpaper);
 
     IShellItemArray *items;
-    hr = CreateShellItemArrayFromPaths(5, paths, &items);
+    hr = CreateShellItemArrayFromPaths(argc - 1, paths, &items);
     if (FAILED(hr)) return ERRORED(__LINE__);
 
     hr = pDesktopWallpaper->SetSlideshow(items);

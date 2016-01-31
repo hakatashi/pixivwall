41f898f4b88e55956a5986911544c451fc0c975c
Koki Takahashi <hakatasiloving@gmail.com>
2016-01-31 13:10:17 +0900

    Make up battery-status submodule to work

--- battery-status/index.cc
+++ battery-status/index.cc
@@ -1,9 +1,19 @@
 #include <nan.h>
+// windows.h is already included in nan.h
 
 using namespace v8;
 
 void GetStatus(const Nan::FunctionCallbackInfo<Value>& info) {
-	info.GetReturnValue().Set(Nan::New("world").ToLocalChecked());
+	LPSYSTEM_POWER_STATUS lpSystemPowerStatus;
+
+	// Get current buttery status
+	GetSystemPowerStatus(lpSystemPowerStatus);
+
+	// Create local v8 number instance pointing buttery status number
+	Local<Int32> localButteryStatusNumber = Nan::New(lpSystemPowerStatus->ACLineStatus);
+
+	// Set return value
+	info.GetReturnValue().Set(localButteryStatusNumber);
 }
 
 void Init(Local<Object> exports) {
